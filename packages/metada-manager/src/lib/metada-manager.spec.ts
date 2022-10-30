6e705af8cdb9d155368e517fbd67fe9b448a82c4
Garrett Myrick <garrettmyrick@gmail.com>
2022-10-30 14:14:49 -0700

    added metadata-manager package

---
+++ packages/metada-manager/src/lib/metada-manager.spec.ts
@@ -0,0 +1,7 @@
+import { metadaManager } from './metada-manager';
+
+describe('metadaManager', () => {
+  it('should work', () => {
+    expect(metadaManager()).toEqual('metada-manager');
+  });
+});
---
+++ packages/metada-manager/src/lib/metada-manager.ts
@@ -0,0 +1,3 @@
+export function metadaManager(): string {
+  return 'metada-manager';
+}
