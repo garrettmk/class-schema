37641b7fbd0b52198cc99f58facf86836792da56
Garrett Myrick <garrettmyrick@gmail.com>
2022-10-30 20:04:10 -0700

    metadata-manager: fixed package name

---
+++ packages/metadata-manager/README.md
@@ -0,0 +1,11 @@
+# metadata-manager
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test metadata-manager` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint metadata-manager` to execute the lint via [ESLint](https://eslint.org/).
--- packages/metadata-manager/jest.config.ts
+++ packages/metadata-manager/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'metada-manager',
+  displayName: 'metadata-manager',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/metada-manager',
+  coverageDirectory: '../../coverage/packages/metadata-manager',
 };
