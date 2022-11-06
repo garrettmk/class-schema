ce5865d0b84551f0ac935f8f8170e55185991a4b
Garrett Myrick <garrettmyrick@gmail.com>
2022-11-06 08:56:49 -0800

    metadata-manager: set up typedoc

--- packages/metadata-manager/src/lib/metadata-manager.ts
+++ packages/metadata-manager/src/lib/metadata-manager.ts
@@ -4,6 +4,26 @@ import { getPrototypeChain } from './util/get-prototype-chain';
 import { merge } from './util/merge';
 
 
+/**
+ * Creates a new class implementing the `MetadataManager` interface. All
+ * methods/properties of this class are static, so it essentially acts
+ * as a "namespace" for a kind of metadata.
+ * 
+ * @param metadatas An initial set of `[Target, Metadata]` entries.
+ * @returns A class implementing the `MetadataManager` interface.
+ * 
+ * @example
+ * ```
+ * const MyMetadataManager = MetadataManagerClass();
+ * ```
+ * 
+ * @example
+ * ```
+ * class MyMetadataManager extends MetadataManagerClass() {
+ *  // Provide custom functionality here
+ * }
+ * ```
+ */
 export function MetadataManagerClass<Metadata extends MetadataDict, Target>(metadatas?: [Target, Metadata][]): Constructor & MetadataManager<Metadata, Target> {
   return class {
     protected static metadatas = new Map<Target, Metadata>(metadatas);
--- packages/metadata-manager/src/lib/types.ts
+++ packages/metadata-manager/src/lib/types.ts
@@ -1,20 +1,41 @@
 /**
- * A class constructor.
+ * A constructor function.
  */
 export type Constructor<T = any>= new () => T;
 
+/**
+ * Represents any function. Preferred over using `Function` as a type.
+ */
 export type AnyFunction = () => any;
 
+/**
+ * A class decorator function.
+ */
 export type ClassDecorator<T> = (target: T) => T | void;
 
+/**
+ * A property key.
+ */
 export type PropertyKey = string | symbol;
 
+/**
+ * A property decorator function.
+ */
 export type PropertyDecorator<T = object> = (target: T, key: PropertyKey) => void;
 
+/**
+ * A valid metadata key.
+ */
 export type MetadataKey = string | symbol;
 
+/**
+ * A metadata dictionary.
+ */
 export type MetadataDict<MetadataField = unknown> = Record<MetadataKey, MetadataField>;
 
+/**
+ * A metadata manager class.
+ */
 export interface MetadataManager<Metadata = MetadataDict, Target = unknown> {
     hasMetadata(target: Target): boolean
     getMetadata(target: Target): Metadata
