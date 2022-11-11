39e16f2f8a403e5135520d0896c47986f6ca4355
Garrett Myrick <garrettmyrick@gmail.com>
2022-11-11 14:59:19 -0800

    add utilities

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/types';
 export * from './lib/ensure-array';
 export * from './lib/get-prototype-chain';
 export * from './lib/merge';
+export * from './lib/flip';
+export * from './lib/list-of'
---
+++ packages/util/src/lib/flip.ts
@@ -0,0 +1,4 @@
+
+export function flip<A, B>(a: A, b: B): A | B {
+    return Math.random() > 0.5 ? a : b;
+}
